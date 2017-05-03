81feef298560b933d7422ef6d8c31e42dc6c5fcb
vildozo <vildozo@outlook.com>
2017-05-03 06:58:37 -0400

    Se agrego hourly commision

--- src/main/java/Presenter/Presentador_empleado.java
+++ src/main/java/Presenter/Presentador_empleado.java
@@ -5,6 +5,7 @@ import org.apache.velocity.app.VelocityEngine;
 import payrollcasestudy.boundaries.PayrollDatabase;
 import spark.template.velocity.VelocityTemplateEngine;
 import payrollcasestudy.entities.Employee;
+import payrollcasestudy.entities.paymentclassifications.HourlyPaymentClassification;
 
 public class Presentador_empleado {
 	public static String empleado() {
@@ -16,18 +17,19 @@ public class Presentador_empleado {
 		
 	}
 	
-	public static String responder_registro( String employee_CI,String nombre,String address,String payment){
+	public static String responder_registro( String employee_CI,String nombre,String address,String payment,String amount){
 		VelocityTemplateEngine vte= new VelocityTemplateEngine();
 		int ci;
 		PayrollDatabase database = PayrollDatabase.globalPayrollDatabase;
 		System.out.println("----------RESPONDIENDO---------");
 		ci= Integer.parseInt(employee_CI);
 		Employee employee= new Employee(ci,nombre,address);
+		//Double.parseDouble(aString)
 		if (payment=="hourly")
-			employee.setPaymentClassification(null);
+			employee.setPaymentClassification(new HourlyPaymentClassification(Double.parseDouble(amount)));
 		if (payment=="commissioned")
 			employee.setPaymentClassification(null);
-		if (payment==salaried)
+		if (payment=="salaried")
 			employee.setPaymentClassification(null);
 		database.addEmployee(ci, employee);
 		vte.asignarDireccionTemplate("src/main/java/Presenter/empleado.vt");
@@ -35,6 +37,7 @@ public class Presentador_empleado {
 		vte.agregarContext("nombre", nombre);
 		vte.agregarContext("address", address);
 		vte.agregarContext("payment", payment);
+		vte.agregarContext("amount", amount);
 		vte.unirAlTemplate();
 		return vte.show_template();//"El nuevo empleado tiene ci= "+ ci + " y su nombre es "+nombre+" y su direccion es "+ address;
 	}
