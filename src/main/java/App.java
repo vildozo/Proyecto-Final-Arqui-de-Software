a701935aec7dd6cfb2d7711df078e0fb55776e07
Lesly Méndez C <leslicita_mc@hotmail.com>
2017-05-09 20:52:24 -0400

    Se elimino codigo basura y renombre de archivo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,8 @@
 import static spark.Spark.*;
 
 import java.util.HashMap;
-import java.util.Map;
 
-import Presenter.Presentador_empleado;
+import Presenter.EmployeePresenter;
 import payrollcasestudy.boundaries.PayrollDatabase;
 import payrollcasestudy.entities.Employee;
 import spark.ModelAndView;
@@ -14,14 +13,14 @@ public class App {
 	public static void main(String[] args) {
 		HashMap<String, Object> model = new HashMap<String, Object>();
 		
-		get("/", (request, response) -> hola());
+		get("/", (request, response) -> "Index");
 		
 		get("/register_employee", (request, response) -> {
 			return new ModelAndView(model, "templates/register_employee.vtl");
 		}, new VelocityTemplateEngine());
 		
 		post("/create_employee", (request, response) -> {
-			Presentador_empleado.createEmployee(request.queryParams("employeeId"),
+			EmployeePresenter.createEmployee(request.queryParams("employeeId"),
 					request.queryParams("name"),request.queryParams("address"),
 					request.queryParams("paymentClassification"),request.queryParams("amount"),
 					request.queryParams("commission"));
@@ -38,24 +37,5 @@ public class App {
 			model.put("employee", employee);
 			return new ModelAndView(model, "templates/show_employee.vtl");
 		}, new VelocityTemplateEngine());
-	}
-	
-	private static String hola() {
-		return "<html>"
-				+ "<body>"
-				+ "<form method='post' action='/hola'>" 
-				+ "<label>Nombre:</label>"
-				+ "<input type='text' name='nombre_saludo'>"
-				+ "<input type='submit' value='Saluda'"
-				+ "</body>"
-				+ "</html>";
-	}
-	
-	private static String MostrarEmpleados(){
-		//queryParams("employee_CI"),request.queryParams("employee_Name"),request.queryParams("address")
-		return "<html>"
-				+ "<body>"
-				+ "</body>"
-				+ "</html>";
 	}
 }
---
+++ src/main/java/Presenter/EmployeePresenter.java
@@ -0,0 +1,21 @@
+package Presenter;
+
+import payrollcasestudy.transactions.Transaction;
+import payrollcasestudy.transactions.add.AddCommissionedEmployeeTransaction;
+import payrollcasestudy.transactions.add.AddHourlyEmployeeTransaction;
+import payrollcasestudy.transactions.add.AddSalariedEmployeeTransaction;
+
+public class EmployeePresenter {
+	
+	private static Transaction addEmployeeTransaction;
+	
+	public static void createEmployee(String employeeId, String name, String address, String paymentClassification, String amount, String commission) {
+		if(paymentClassification.equals("hourly"))
+			addEmployeeTransaction = new AddHourlyEmployeeTransaction(Integer.parseInt(employeeId), name, address, Double.parseDouble(amount));
+		if(paymentClassification.equals("commissioned"))
+			addEmployeeTransaction = new AddCommissionedEmployeeTransaction(Integer.parseInt(employeeId), name, address, Double.parseDouble(amount), Double.parseDouble(commission));
+		if(paymentClassification.equals("salaried"))
+			addEmployeeTransaction = new AddSalariedEmployeeTransaction(Integer.parseInt(employeeId), name, address, Double.parseDouble(amount));
+		addEmployeeTransaction.execute();
+	}
+}
